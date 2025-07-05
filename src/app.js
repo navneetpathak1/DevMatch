2f31ab94bcca222d2d52296bc814d9ee5d34665f
Navneet Pathak <navneetpathak1909@gmail.com>
2025-07-05 22:54:31 +0530

    init

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+
+app.use("/", (req,res) => {
+    res.send("Hello");
+})
+
+
+app.listen(3000, () =>{
+    console.log("server is running on port no 3000...")
+})
